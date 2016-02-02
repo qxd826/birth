7b857052786456f506584b94f5467723addc61d7
xiangdong.qu <qxd826@163.com>
2016-02-02 16:51:04 +0800

    quartz定时器配置 springContextUtil上下文环境

---
+++ birth.biz/src/main/java/com/qxd/birth/biz/quartz/TestQuartz.java
@@ -0,0 +1,15 @@
+package com.qxd.birth.biz.quartz;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by xiangDong.qu on 16/2/2.
+ */
+@Component
+@Slf4j
+public class TestQuartz {
+    public void execute(){
+        log.info("[定时任务测试] I am quartz");
+    }
+}
