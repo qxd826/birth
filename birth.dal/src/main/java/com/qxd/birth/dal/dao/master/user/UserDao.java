bdf7407ce7da4357b4cc09de181f2b77a3d1d087
xiangdong.qu <qxd826@163.com>
2016-02-17 16:08:44 +0800

    mybatis 测试

---
+++ birth.dal/src/main/java/com/qxd/birth/dal/dao/master/user/UserDao.java
@@ -0,0 +1,13 @@
+package com.qxd.birth.dal.dao.master.user;
+
+import com.qxd.birth.dal.dao.base.BaseDao;
+import com.qxd.birth.dal.dao.common.MyBatisRepository;
+import com.qxd.birth.dal.entity.master.user.User;
+
+/**
+ * Created by xiangDong.qu on 16/2/17.
+ */
+@MyBatisRepository
+public interface UserDao extends BaseDao<User> {
+
+}
---
+++ birth.dal/src/main/java/com/qxd/birth/dal/entity/master/user/User.java
@@ -0,0 +1,17 @@
+package com.qxd.birth.dal.entity.master.user;
+
+import com.qxd.birth.dal.entity.base.BaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Created by xiangDong.qu on 16/2/17.
+ */
+@EqualsAndHashCode(callSuper = false)
+@Data
+public class User extends BaseEntity {
+
+    private String name;//用户名
+    private String mobile;//用户电话号码
+
+}
